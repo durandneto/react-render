0dda3a78d7d2f0bc8ad84bd9549d1b97220677c8
Durand Neto <durand.neto@gmail.com>
2017-02-06 17:22:09 -0200

    first version

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import { Home } from 'oilib'
+
+class App extends Component {
+
+  render() {
+	return (
+		<Home content='wrapper'>
+			Home content
+		</Home>
+	);
+  }
+}
+
+export { App }
+export default App
